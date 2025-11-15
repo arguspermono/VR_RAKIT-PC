68e9cf15902df93291d448fcd928f25fc662c9c4
Arya Bagus <Argusknight10@gmail.com>
2025-11-15 22:39:56 +0700

    fixing error on mainmenu part-1

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -2,13 +2,17 @@ import { attachInteractions } from "./interactions.js"
 
 export async function createMainMenu({ scene, onStart }) {
 
+  // MANAGER GUI 3D
   const manager = new BABYLON.GUI.GUI3DManager(scene);
 
+  // PANEL 3D
   const panel = new BABYLON.GUI.StackPanel3D();
   manager.addControl(panel);
 
+  // Letakkan panel di depan kamera
   panel.position = new BABYLON.Vector3(0, 1.5, 2);
 
+  // BUTTON START
   const startBtn = new BABYLON.GUI.HolographicButton("startBtn");
   startBtn.text = "Start";
   panel.addControl(startBtn);
@@ -20,4 +24,5 @@ export async function createMainMenu({ scene, onStart }) {
 
   return panel;
 }
+
 
