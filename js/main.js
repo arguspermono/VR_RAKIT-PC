64ad650486b47aa9fe7eb26199a5cee75861adee
Arya Bagus <Argusknight10@gmail.com>
2025-11-15 21:54:17 +0700

    delete comment

--- js/main.js
+++ js/main.js
@@ -1,34 +1,3 @@
-// import { createScene } from "./scene.js"
-// import { createMainMenu } from "./mainmenu.js"
-
-// const canvas = document.getElementById("renderCanvas");
-// const engine = new BABYLON.Engine(canvas, true);
-
-// // Ambil elemen menu
-// const menu = document.getElementById("mainMenu");
-// const startBtn = document.getElementById("startBtn");
-
-// let scene = null;
-
-// // Jangan buat scene apapun sebelum tombol diklik
-
-// startBtn.addEventListener("click", async () => {
-//   // Hide main menu
-//   menu.style.display = "none";
-
-//   // Create the real scene
-//   scene = await createScene(engine, canvas);
-
-//   engine.runRenderLoop(() => {
-//     scene.render();
-//   });
-// });
-
-// // Resize handler
-// window.addEventListener("resize", () => {
-//   engine.resize();
-// });
-
 import { createMainMenu } from "./mainmenu.js";
 import { createScene } from "./scene.js";
 
