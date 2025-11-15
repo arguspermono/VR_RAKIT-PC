b0f94e5a23fde6ed2aec1490f6454c6a2fa60db9
Arya Bagus <Argusknight10@gmail.com>
2025-11-15 21:53:09 +0700

    adjust mainmenu to have access with vr

--- js/main.js
+++ js/main.js
@@ -1,29 +1,52 @@
+// import { createScene } from "./scene.js"
+// import { createMainMenu } from "./mainmenu.js"
+
+// const canvas = document.getElementById("renderCanvas");
+// const engine = new BABYLON.Engine(canvas, true);
+
+// // Ambil elemen menu
+// const menu = document.getElementById("mainMenu");
+// const startBtn = document.getElementById("startBtn");
+
+// let scene = null;
+
+// // Jangan buat scene apapun sebelum tombol diklik
+
+// startBtn.addEventListener("click", async () => {
+//   // Hide main menu
+//   menu.style.display = "none";
+
+//   // Create the real scene
+//   scene = await createScene(engine, canvas);
+
+//   engine.runRenderLoop(() => {
+//     scene.render();
+//   });
+// });
+
+// // Resize handler
+// window.addEventListener("resize", () => {
+//   engine.resize();
+// });
+
+import { createMainMenu } from "./mainmenu.js";
 import { createScene } from "./scene.js";
 
 const canvas = document.getElementById("renderCanvas");
 const engine = new BABYLON.Engine(canvas, true);
 
-// Ambil elemen menu
-const menu = document.getElementById("mainMenu");
-const startBtn = document.getElementById("startBtn");
+let scene = new BABYLON.Scene(engine);
 
-let scene = null;
-
-// Jangan buat scene apapun sebelum tombol diklik
-
-startBtn.addEventListener("click", async () => {
-  // Hide main menu
-  menu.style.display = "none";
-
-  // Create the real scene
-  scene = await createScene(engine, canvas);
-
-  engine.runRenderLoop(() => {
-    scene.render();
-  });
+// tampilkan main menu
+await createMainMenu({
+  scene,
+  onStart: async () => {
+    scene = await createScene(engine, canvas);
+  }
 });
 
-// Resize handler
-window.addEventListener("resize", () => {
-  engine.resize();
+engine.runRenderLoop(() => {
+  scene.render();
 });
+
+window.addEventListener("resize", () => engine.resize());
